546234e625c667021436fbd73824eb63e23ae389
salvafern <salvador.fernandez@vliz.be>
2022-09-26 15:56:48 +0200

    last update

--- R/Datacleaning_standardization.R
+++ R/Datacleaning_standardization.R
@@ -2,32 +2,31 @@
 # Data cleaning and standardisation in R 
 # Laura Marquez & Salvador Fernandez
 
+# make sure to have installed tidyverse, uncomment next to install it
+# install.packages("tidyverse")
 library(tidyverse)
 
-DataAbundancew <- read_csv("./data/DataAbundancew.csv")
-
-#Exercises part A. (10 min)
-
+# Exercises part A. (10 min)
+# cheatsheet: https://github.com/rstudio/cheatsheets/blob/main/strings.pdf
 # 1. Open the file and look at the scope of the data, something strange with the site names? *use unique() 
 
-
 # 2. Identify mistyping errors in the site names (Ballylongfor, New Quay_, New quay, newquay) and correct them *use str_replace()
-
 
 # 3. Delete special characters and spaces from the data *use str_replace()
 
 
 # Exercises part B. (15 min)
-# 1. Standardize the dates in IS0 8601 standard (YYYY-mm-dd) *use separate() & mutate()
+# cheatsheet: https://nyu-cdsc.github.io/learningr/assets/data-transformation.pdf
+# cheatsheet: https://raw.githubusercontent.com/rstudio/cheatsheets/main/tidyr.pdf
 
+# 1. Standardize the dates in IS0 8601 standard (YYYY-mm-dd) *use separate() & mutate()
 
 # 2. Use pivot function to get your data tidy (one observation per row) *use pivot_longer()
 
-
 # 3. Drop the unnecessary columns (day, month, year, SiteName) *use select() 
 
-
 # Great job! Now your data is tidy, we can move on standardization!
+
 
 # Exercises part C. (5 min)
 # 1. Modify DataAbundancew column names to fit the Darwin Core standard * use rename()
@@ -36,20 +35,24 @@ DataAbundancew <- read_csv("./data/DataAbundancew.csv")
 # Exercises part D. (10 min)
 # 1. Use worrms library to obtain the LSID of each species and put them in a new column named “scientificNameID”
 #    * use wm_records_taxamatch
-#    * once you obtained them, use “do.call(rbind, myspeciestable)” 	to transform the tibble to data frame 
+#    * once you obtained them, use “do.call(rbind, myspeciestable)”	to extract the elements of myspeciestable  
 #    * use left_join to ONLY add the species 
-
-#https://github.com/ropensci/worrms
-#https://cran.r-project.org/web/packages/worms/worms.pdf
+# https://cran.r-project.org/web/packages/worrms/worrms.pdf
+# uncomment next to install worrms R package:
+# install.packages("worrms")
 
 library(worrms)
 
 myspeciestable <- wm_records_taxamatch(unique(YOURDATAHERE)) 
+
 table <- do.call(rbind, myspeciestable)
 
 
 # Exercises part E. (10 min)
 # 1. Use marineregions to search for the correct id of the region and put it in a new column (locationID) 
 # https://www.marineregions.org/gazetteer.php?p=search
-# https://github.com/ropensci/mregions
+# uncomment next to install mregions2 R package:
+# install.packages("devtools")
+# devtools::install_github("lifewatch/mregions2", build_vignettes = TRUE)
 
+library(mregions2)
--- R/Datacleaning_standardization_solution.R
+++ R/Datacleaning_standardization_solution.R
@@ -1,32 +1,31 @@
-#Empowering Biodiversity Research
-#Data cleaning and standardisation in R 
+# Empowering Biodiversity Research
+# Data cleaning and standardisation in R 
 
+# make sure to have installed tidyverse, uncomment next to install it
+# install.packages("tidyverse")
 library(tidyverse)
 
 DataAbundancew <- read_csv("./data/DataAbundancew.csv")
 
 
-#Exercises part A. (10 min)
-
+# Exercises part A. (10 min)
+# cheatsheet https://github.com/rstudio/cheatsheets/blob/main/strings.pdf
 # 1. Open the file and look at the scope of the data, something strange with the site names? *use unique() 
+# It is always a good practice to look at the scope of your data, to check for possible mistyping errors, outliers OR na (always make sure these are real NA and not 0 or viceverse) & decide how to handle them
 unique(DataAbundancew$date)
 unique(DataAbundancew$SiteName) #is there anything that do you notice here? (there are mistyping errors)
 
-unique(DataAbundancew$Latitude)
-unique(DataAbundancew$Longitude)#it is always a good practice to look at the scope of your data, to check for possible mistyping errors, outliers OR na (always make sure these are real NA and not 0 or viceverse) & decide how to handle them
-
+range(DataAbundancew$Latitude) 
 
 # 2. Identify mistyping errors in the site names and correct them *use str_replace()
 # Replace the mistyping in the data: Ballylongfor, New Quay_, New quay, newquay
-# !! Note that is always a good practice to preserve the original data and create a second column containing the corrections
+# ! Note that is always a good practice to preserve the original data and create a second column containing the corrections
 DataAbundancew$SiteName_clean <- DataAbundancew$SiteName
-names(DataAbundancew)
 
-unique(DataAbundancew$SiteName_clean)
 DataAbundancew$SiteName_clean <- str_replace(DataAbundancew$SiteName_clean, "New quay", "New Quay") #ok
 DataAbundancew$SiteName_clean <- str_replace(DataAbundancew$SiteName_clean, "newquay", "New Quay") #ok
 DataAbundancew$SiteName_clean <- str_replace(DataAbundancew$SiteName_clean, "New Quay_", "New Quay") #ok
-str_replace(DataAbundancew$SiteName_clean, "Ballylongfor", "Ballylongford") #
+str_replace(DataAbundancew$SiteName_clean, "Ballylongfor", "Ballylongford") #note here using str_replace does not work for our purpose
 
 DataAbundancew$SiteName_clean[DataAbundancew$SiteName_clean == 'Ballylongfor'] <- "Ballylongford"
 
@@ -40,20 +39,35 @@ DataAbundancew$SiteName_clean <- str_replace(DataAbundancew$SiteName_clean, " ",
 
 
 # Now we are going to present 4 important functions from tidyverse that are key to do data cleaning & standardization
-# this are useful not only for this, but for handling any type of data
 # Using %>%, introduce select, filter, mutate, transform
+# cheatsheet: https://nyu-cdsc.github.io/learningr/assets/data-transformation.pdf
 DataAbundancew %>% select(SiteName,
                           Latitude,
                           Longitude)
 
 DataAbundancew %>% filter(date == "2/1/2019")
 
-DataAbundancew %>% mutate(SiteNameCountry = paste0(SiteName_clean, ", Ireland"))
+dt<- DataAbundancew %>% mutate(SiteNameCountry = paste0(SiteName_clean, ", Ireland"))
 
-DataAbundancew %>% transmute(SiteNameCountry = paste0(SiteName_clean, ", Ireland"))
+dt<- DataAbundancew %>% transmute(SiteNameCountry = paste0(SiteName_clean, ", Ireland"))
 
+#you could also correct all mistakes from one column (exercises A.2 & A.3) with one piece of code:
+DataAbundancew <- DataAbundancew %>% 
+                                  mutate(
+                                    SiteName = SiteName %>% 
+                                      tolower() %>% 
+                                      trimws() %>% 
+                                      str_replace("_", "") %>% 
+                                      str_replace("'", "") %>%
+                                      str_replace(",", "") %>%
+                                      str_replace("ballylongford", "ballylongfor") %>%
+                                      str_replace("ballylongfor", "ballylongford") %>%
+                                      str_replace(" ", "")
+                                  )
 
 # Exercises part B. (15 min)
+# cheatsheet https://raw.githubusercontent.com/rstudio/cheatsheets/main/tidyr.pdf
+
 # 1. Standardize the dates in IS0 8601 standard (YYYY-mm-dd) *use separate() & mutate()
 DataAbundancew <- DataAbundancew %>% separate(col = date, 
                                              into = c("month","day", "year"),
@@ -85,47 +99,55 @@ DataAbundancew <- DataAbundancew %>% select(-c("month", "day", "year", "SiteName
 
 # Exercises part C. (5 min)
 # 1. Modify DataAbundancew column names to fit the Darwin Core standard * use rename()
-DataAbundancew <- DataAbundancew %>% rename(
-  recordNumber = observationNumber,
-  eventDate = date,
-  decimalLatitude = Latitude,
-  decimalLongitude = Longitude,
-  Location = SiteName_clean,
-  scientificName = species,
-  individualCount = abundance
-)
+DataAbundancew <- DataAbundancew %>% rename(recordNumber = observationNumber,
+                                            eventDate = date,
+                                            decimalLatitude = Latitude,
+                                            decimalLongitude = Longitude,
+                                            locality = SiteName_clean,
+                                            scientificName = species,
+                                            individualCount = abundance
+                                          )
 
 
 # Exercises part D. (10 min)
 # 1. Use worrms library to obtain the LSID of each species and put them in a new column named “scientificNameID”
 #    * use wm_records_taxamatch
-#    * once you obtained them, use “do.call(rbind, myspeciestable)” 	to transform the tibble to data frame 
+#    * once you obtained them, use “do.call(rbind, myspeciestable)” to extract the elements of myspeciestable 
 #    * use left_join to ONLY add the species 
-
-#https://github.com/ropensci/worrms
-#https://cran.r-project.org/web/packages/worms/worms.pdf
+# https://cran.r-project.org/web/packages/worrms/worrms.pdf
+# uncomment next to install worrms R package:
+# install.packages("worrms")
 
 library(worrms)
+myspeciestable <- wm_records_taxamatch(unique(DataAbundancew$scientificName)) 
 
-table <-wm_records_taxamatch(unique(DataAbundancew$scientificName)) #join introducing
-table <-do.call(rbind, table)
+#transform the list of dataframes to a single dataframe
+table <- do.call(rbind, myspeciestable) %>%
+                         select(scientificname, lsid)
 
-speciesID <- table %>% select(scientificName= scientificname,
-                              lsid)
-
-DataAbundancew <- left_join(DataAbundancew, speciesID, by = "scientificName")
-
+DataAbundancew <- DataAbundancew %>%
+                   left_join(table, by = c("scientificName" = "scientificname"))
 
 # Exercises part E. (10 min)
 # 1. Use marineregions to search for the correct id of the region and put it in a new column (locationID) 
 # https://www.marineregions.org/gazetteer.php?p=search
-# https://github.com/ropensci/mregions
+# uncomment next to install mregions2 R package:
+# devtools::install_github("lifewatch/mregions2", build_vignettes = TRUE)
 
+library(mregions2)
 DataAbundancew %>% 
-  sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, remove = FALSE) %>% 
+  sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), crs = 4326, remove = FALSE) %>% 
   mapview::mapview()
 
-mregions2::mr_gaz_records_by_name("Irish") %>% filter(placeType == "EEZ")
+irish_mr <- mr_gaz_records_by_name("Irish") 
+
+irish_mr %>% distinct(placeType)
+irish_mr %>% View()
+
+irish_eez <- irish_mr %>%
+  filter(placeType == "EEZ")
+
+irish_eez$MRGID
 
 DataAbundancew <- DataAbundancew %>% mutate(locationID = "http://marineregions.org/mrgid/5681")
 
